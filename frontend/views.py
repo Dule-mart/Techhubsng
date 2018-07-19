0f002c4a48103233700ba6fbe09c31837e6ececa
admin <admin@admins-MacBook-Pro.local>
2018-07-19 20:23:41 +0100

    remove all

--- frontend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    text = {"text": "this is the home page" }
-    return render(request, "frontend/home.html", text)
